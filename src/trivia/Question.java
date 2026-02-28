554de96dc90baf53b9abe2ee088f84e557073fe6
Aloyse ARNAUD <aloyse.arnaud@etu.u-bordeaux.fr>
2026-02-28 16:17:02 +0100

    add the instruction sentence of ORDERING

--- src/trivia/Question.java
+++ src/trivia/Question.java
@@ -12,6 +12,7 @@ public class Question {
     private String answer; // used for MCQ, TF, Open-Ended
     private double numericAnswer; // used for NUMERIC
     private double tolerance; // margin of error for NUMERIC -> is going to be used for estimation-based challenges
+    private List<String> orderingAnswer; // used for ORDERING
 
     public Question() {} // empty constructor for GSON
 
@@ -39,6 +40,17 @@ public class Question {
             sb.append("[Answer with a number]\n");
         } else if (type == QuestionType.OPEN_ENDED) {
             sb.append("Type an answer\n");
+        } else if (type == QuestionType.ORDERING) {
+            if (choices != null) {
+                int index = 1;
+                for (String choice : choices) {
+                    sb.append(index).append(") ").append(choice).append("\n");
+                    index++;
+                }
+                sb.append(
+                    "Enter the correct order (example: 2 1 3 or 1;2;3)\n"
+                );
+            }
         }
         return sb.toString();
     }
@@ -70,5 +82,9 @@ public class Question {
 
     public double getTolerance() {
         return tolerance;
+    }
+
+    public List<String> getOrderingAnswer() {
+        return orderingAnswer;
     }
 }
