189a2286379cdb6bacc19e8ee1dcb6ca5ca2010c
Aloyse ARNAUD <aloyse.arnaud@etu.u-bordeaux.fr>
2026-03-24 16:59:13 +0100

    Enum of the type of weaspon

---
+++ src/game/WeaponType.java
@@ -0,0 +1,10 @@
+package game;
+
+public enum WeaponType {
+    CANNON, // increases difficulty of opponent's question
+    CROSSBOW, // choose opponent's question category
+    BURST, // opponent has only 7 seconds to answer
+    SHIELD, // cancels opponent's attack
+    LASER_SIGHT, // choose your own question category
+    HELMET, // lowers difficulty of your question
+}
