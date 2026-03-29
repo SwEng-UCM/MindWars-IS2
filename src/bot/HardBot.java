bca7d80c14ab52cc2eca5acdc1993647090f0c86
elenathea3103 <elenaungureanu3103@gmail.com>
2026-03-29 16:54:43 +0200

    started working on #69

--- src/bot/HardBot.java
+++ src/bot/HardBot.java
@@ -1,16 +1,28 @@
 package bot;
 
 import trivia.Question;
+import java.util.Random;
 
 public class HardBot implements BotStrategy {
+    private final Random random = new Random();
+
     @Override
     public String getAnswer(Question question) {
-        return question.getAnswer();
+        int chance = random.nextInt(100);
+
+        // 80% chances to give the right answer
+        if (chance < 80) {
+            return question.getAnswer();
+        } else {
+            // 20% chances to give a wrong answer (not finished bcs there are multiple types
+            // of questions)
+            return "I am not sure, but I will guess A";
+        }
     }
 
     @Override
     public long getResponseTime() {
-        return 2000; // 2 seconds
+        return 1000 + random.nextInt(2000);
     }
 
     @Override
