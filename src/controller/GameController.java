2a319327f8665fab720d49481d278ecba0100db3
anastasiachelaru <chelaruanastasia6@gmail.com>
2026-04-13 20:23:59 +0200

    fixed issue #105

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -95,7 +95,6 @@ public class GameController {
             } else {
                 model.beginQuestion();
             }
-
         } else if (phase == GamePhase.INVASION_PASS) {
             model.beginInvasionSelect();
         }
