a1a1b5a8ba48084915b77f3371867fad56601716
ashleyumeghalu <ashleyumeghalu@yahoo.com>
2026-04-16 15:43:06 +0200

    made repositories for save game, load and leaderbord #80

---
+++ src/persistence/LeaderBoardRepository.java
@@ -0,0 +1,42 @@
+package persistence;
+
+import player.Player;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class LeaderBoardRepository {
+
+    public void saveMatchResults(Player player, boolean won, int territoriesOwned,String gameMode, int mapSize){
+        String query = """
+                INSERT INTO leaderboard_entries
+                (user_id, score, territories_owned, correct_answers, wrong_answers,
+                             average_response_time_ms, fastest_response_time_ms, won, game_mode, map_size)
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """;
+
+        try (Connection conn =DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)
+        ) {
+            if (player.getUserID() == null) {
+                throw new IllegalArgumentException("Player has no userId. Cannot save leaderboard entry.");
+            }
+            stmt.setInt(1, player.getUserID());
+            stmt.setInt(2, player.getScore());
+            stmt.setInt(3, territoriesOwned);
+            stmt.setInt(4, player.getCorrectAnswers());
+            stmt.setInt(5, player.getWrongAnswers());
+            stmt.setDouble(6, player.getAverageResponseTime());
+            stmt.setDouble(7, player.getFastestResponse());
+            stmt.setInt(8, won ? 1 : 0);
+            stmt.setString(9, gameMode);
+            stmt.setInt(10, mapSize);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to save leaderbord entry",e);
+        }
+    }
+}
---
+++ src/persistence/SaveGameRepository.java
@@ -0,0 +1,51 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SaveGameRepository {
+
+    public void saveGame(
+            String saveName,
+            int player1UserId,
+            int player2UserId,
+            Integer currentTurnUserId,
+            int mapSize,
+            String gameMode,
+            String phase,
+            String gameStateJson
+    ){
+        String query =  """
+            INSERT INTO saved_games
+            (save_name, player1_user_id, player2_user_id, current_turn_user_id,
+             map_size, game_mode, phase, game_state_json)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """;
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query);
+        ){
+            stmt.setString(1, saveName);
+            stmt.setInt(2, player1UserId);
+            stmt.setInt(3, player2UserId);
+
+            if (currentTurnUserId == null) {
+                stmt.setNull(4, java.sql.Types.INTEGER);
+            } else {
+                stmt.setInt(4, currentTurnUserId);
+            }
+
+            stmt.setInt(5, mapSize);
+            stmt.setString(6, gameMode);
+            stmt.setString(7, phase);
+            stmt.setString(8, gameStateJson);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to save game",e);
+        }
+
+    }
+}
