80159ea7f5f2de1fab6c74607e616957647da877
Aloyse ARNAUD <aloyse.arnaud@etu.u-bordeaux.fr>
2026-04-19 19:11:48 +0200

    Login controller

---
+++ src/controller/LoginController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import model.User;
+import persistence.PasswordUtil;
+import persistence.UserRepository;
+import player.Player;
+import ui.MenuPanel;
+
+public class LoginController {
+
+    private MenuPanel ui;
+    private UserRepository repo;
+
+    public LoginController(MenuPanel ui, UserRepository repo) {
+        this.ui = ui;
+        this.repo = repo;
+    }
+
+    public void login() {
+        String email = ui.getEmailInput();
+        String password = ui.getPasswordInput();
+
+        String hash = PasswordUtil.hashPassword(password);
+
+        User user = repo.login(email, hash);
+
+        if (user == null) {
+            ui.showError("Invalid email or password");
+            return;
+        }
+
+        Player player = new Player(user.getUsername(), user.getId());
+        ui.getParentWindow().setSessionPlayer(player);
+
+        ui.showSuccess("Welcome " + user.getUsername());
+
+        ui.getParentWindow().startGameSession();
+    }
+}
